1549fbd5a0158c5d666d2392358efe6a2a638b87
Dinh Van Thanh <develooper2022@gmail.com>
2022-08-25 14:50:06 +0700

    v1.0.5

--- app.js
+++ app.js
@@ -2,23 +2,28 @@ const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
 
-$('.icon-menu').onclick = function() {
-    $('.header__right').style.display = 'flex'
-    $('.icon-menu').style.display = 'none'
+$('.icon-menu').onclick = () => {
+    $('.icon-menu').classList.add('active')
+    $('.header__right').classList.add('active')
+    $('.btn-close.active').classList.remove('active')
 }
 
-$('.btn-close').onclick = function() {
-    $('.header__right').style.display = 'none'
-    $('.icon-menu').style.display = 'block'
+$('.btn-close').onclick = () => {
+    $('.btn-close').classList.add('active')
+    $('.header__right.active').classList.remove('active')
+    $('.icon-menu.active').classList.remove('active')
 }
 
-$$('.menu-link').forEach((el, i) => {
-    el.addEventListener('click', () => {
-        closeMenu()
-    })
-});
+// remove tag <br/>
+console.log()
 
-function closeMenu() {
-    $('.header__right').style.display = 'none'
-    $('.icon-menu').style.display = 'block'
-}
+;(() => {
+    if(screen.width < 768) {
+        let result = $('.menu-description h3').innerHTML.split('<br>\n')
+        let htmls = ''
+        let txt = result.forEach(text => {
+            htmls += text.trim() + ' '
+        });
+        $('.menu-description h3').innerText = htmls
+    } 
+})()
